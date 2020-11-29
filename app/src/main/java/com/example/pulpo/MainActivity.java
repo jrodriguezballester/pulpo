3da3ad81ceb75910dfe0423c7cd0e1fd159ad40e
Jose Rodriguez <albertorodriguezsorli@gmail.com>
2020-11-29 10:28:39 +0100

    Cuenta atras

--- app/src/main/java/com/example/pulpo/MainActivity.java
+++ app/src/main/java/com/example/pulpo/MainActivity.java
@@ -1,38 +1,62 @@
 package com.example.pulpo;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    Thread crono;
+    int hora, minutos, segundos, segundosCrono = 0, minutosCrono = 0;
+    CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int hora, minutos, segundos;
+
         Calendar calendario = Calendar.getInstance();
 
-        TextView textView1 = findViewById(R.id.textView1);
+        final TextView textView1 = findViewById(R.id.textView1);
+        Button btnCancel=findViewById(R.id.btn_cancel);
+        Button btnStart=findViewById(R.id.btn_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.start();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+            }
+        });
 
         hora = calendario.get(Calendar.HOUR_OF_DAY);
         minutos = calendario.get(Calendar.MINUTE);
         segundos = calendario.get(Calendar.SECOND);
 
-        textView1.setText(hora + ":" + minutos);
-
-       int segundosP;
-       for (segundosP = 0; segundosP < 80; segundosP++) {
-            textView1.setText(String.valueOf(segundosP));
-
-        }
-
-    }
+        //  textView1.setText(hora + ":" + minutos);
 
 
 
+       countDownTimer = new CountDownTimer(80000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                textView1.setText(String.format(Locale.getDefault(), "%d sec.", millisUntilFinished / 1000L));
+            }
+            public void onFinish() {
+                textView1.setText("Done.");
+            }
+        }.start();
 
+
+//
     }
 }
